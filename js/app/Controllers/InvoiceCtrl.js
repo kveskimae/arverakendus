f044a5e15b0bba8aa663e85e64dc7e5c134c6bf9
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2015-05-15 12:02:07 +0300

    Add application files

---
+++ js/app/Controllers/InvoiceCtrl.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    angular.module('InvoiceApp').controller('InvoiceCtrl', function ($timeout, $rootScope, $scope, $controller, events) {
+        angular.extend(this, $controller('BaseCtrl', {$scope: $scope}));
+        $scope.upload = function (files) {
+            $scope.reset();
+            $scope.appData.extractionInProgress = true;
+            $rootScope.$broadcast(events.message._SEND_TO_PROCESS_, [files]);
+        };
+    });
+})();
